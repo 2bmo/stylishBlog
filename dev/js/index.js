8adbf2185e7fd2f737cdc4700f787d13893e699d
Valerie V <heyfilky@gmail.com>
2017-02-08 17:17:14 +0300

    +all small widths

--- dev/js/index.js
+++ dev/js/index.js
@@ -25,4 +25,18 @@ $(document).ready(function(){
         $('.left-col .headlines').after($('.right-col .left-right'));
         $('.comments').wrap('<div class="left-right"></div>').after($('.archives'));
     }
+    if (windowWidth < 820) {
+        $('.page-title form').before('<button id="toggle-menu">Меню</button>');
+        $('#toggle-menu').click(function () {
+            $('.navigation').toggle(400);
+        });
+        $('.photos li').addClass('photos__photo_upper');
+    }
+    if (windowWidth < 480) {
+        $('.slick').remove();
+        $('.page-title form').after($('#toggle-menu'));
+    }
+    for (let i=0; i < newsHeaders.length; i++) {
+        newsHeaders[i].innerHTML = "Рафаэль Надаль проиграл сотой...";
+    }
 });
